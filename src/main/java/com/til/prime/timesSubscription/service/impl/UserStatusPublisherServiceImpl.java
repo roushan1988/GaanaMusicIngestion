3bebd7467713cfcda85a2d72640f4250848053d1
Roushan Singh <roushan.singh1@timesinternet.in>
2018-05-28 12:47:22 +0530

    Revert "[TO REVERT] temp disable of status publish to kafka"
    This reverts commit 418c8be509ed839fa05e365b7b54afb2f68c47a8.

--- src/main/java/com/til/prime/timesSubscription/service/impl/UserStatusPublisherServiceImpl.java
+++ src/main/java/com/til/prime/timesSubscription/service/impl/UserStatusPublisherServiceImpl.java
@@ -23,16 +23,14 @@ public class UserStatusPublisherServiceImpl implements UserStatusPublisherServic
 
     @Override
     public boolean publishUserStatus(PublishedUserStatusDTO msg) {
-//        try{
-//            LOG.info("Reached here");
-//            kafkaTemplate.send(kafkaTopic, msg).get();
-//            LOG.info("Message published, topic: "+kafkaTopic+", message: "+msg);
-//            return true;
-//        }catch (Exception e){
-//            LOG.error("Exception while publishing message: "+msg, e);
-//            return false;
-//        }
-        return true;
+        try{
+            kafkaTemplate.send(kafkaTopic, msg).get();
+            LOG.info("Message published, topic: "+kafkaTopic+", message: "+msg);
+            return true;
+        }catch (Exception e){
+            LOG.error("Exception while publishing message: "+msg, e);
+            return false;
+        }
     }
 
     /*Below is only sample demo code, this system is not doing any consumption of kafka messages*/
