03e8828a7308cb72193c5d0bb224121333677f92
Roushan Singh <roushan.singh1@timesinternet.in>
2018-06-19 15:54:04 +0530

    enable async

--- src/main/java/com/til/prime/timesSubscription/config/AppConfig.java
+++ src/main/java/com/til/prime/timesSubscription/config/AppConfig.java
@@ -2,12 +2,14 @@ package com.til.prime.timesSubscription.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
+@EnableAsync
 public class AppConfig {
 
 	@Bean
